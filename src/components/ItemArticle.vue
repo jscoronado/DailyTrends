27b9f8eca01244fb4f871e870bbc3a9919fe4a35
Jose <jose@wedocreatives.com>
2021-01-24 20:05:48 +0100

    Creación vista con el listado de noticias para poder editar y borrar

--- src/components/ItemArticle.vue
+++ src/components/ItemArticle.vue
@@ -1,7 +1,9 @@
 <template>
-  <article class="item-article col-12 col-md-3 col-lg-4">
+  <article class="item-article col-12 col-md-3 col-lg-4 d-inline-flex">
     <div class="content-article">
-      <a :href="source" target="_blank"><img :src="image" class="image-article"></a>
+      <div class="image-article">
+        <a :href="source" target="_blank"><img :src="image" :alt="title"></a>
+      </div>
       <span class="publisher-article">{{publisher}}</span>
       <h3 class="title-article"><a :href="source" target="_blank">{{title}}</a></h3>
       <div class="desc-article">{{body}}</div>
---
+++ src/views/ListNews.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id="list-news">
+        <HeaderTitle
+            title="Listado de Noticias"
+        />
+        <div class="container">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Periódico</th>
+                    <th>Acciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="article in Articles" :key="article.key">
+                    <td>{{ article.title }}</td>
+                    <td>{{ article.publisher }}</td>
+                    <td>
+                        <router-link :to="{name: 'edit', params: { id: article.key }}" class="btn btn-app btn-edit"><i class="fas fa-edit"></i>
+                        </router-link>
+                        <button @click.prevent="deleteUser(article.key)" class="btn btn-app btn-delete"><i class="far fa-trash-alt"></i></button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {db} from "../services/firebase";
+    import HeaderTitle from "../components/HeaderTitle";
+
+    export default {
+        name: "ListNews",
+        components: {
+            HeaderTitle
+        },
+
+        data() {
+            return {
+                Articles: []
+            }
+        },
+        created() {
+            db.collection('news').onSnapshot((snapshotChange) => {
+                this.Articles = [];
+                snapshotChange.forEach((doc) => {
+                    this.Articles.push({
+                        key: doc.id,
+                        title: doc.data().title,
+                        body: doc.data().body,
+                        image: doc.data().image,
+                        source: doc.data().source,
+                        publisher: doc.data().publisher
+                    })
+                });
+            })
+        },
+        methods: {
+            deleteUser(id){
+                if (window.confirm("¿Deseas eliminar esta noticia?")) {
+                    db.collection("news").doc(id).delete().then(() => {
+                        console.log("Document deleted!");
+                    })
+                        .catch((error) => {
+                            console.error(error);
+                        })
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
