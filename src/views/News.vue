e8ba1674691c5dfd5a0817b9d5edcdadac502a26
Jose <jose@wedocreatives.com>
2021-01-24 19:58:41 +0100

    Implementación News.vue para el mostrado de noticias creadas y guardadas en firebase

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,33 +1,20 @@
 <template>
   <main id="news" class="page-app page-news">
     <HeaderTitle
-      title="Últimas Noticias"
-      description="Infórmate sobre las últimas noticias de tus periódicos preferidos"
+      title="Mis Noticias"
+      description="Infórmate sobre las últimas noticias"
     />
     <div class="container-news">
       <div class="container">
-        <h4>News</h4>
         <div class="row list-news">
           <ItemArticle
-            title="Artículo 1"
-            body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-            image="https://comercializadora.electricadecadiz.es/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png"
-            source="https://www.google.es"
-            publisher="El País"
-          />
-          <ItemArticle
-                  title="Artículo 2"
-                  body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-                  image="https://comercializadora.electricadecadiz.es/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png"
-                  source="https://www.google.es"
-                  publisher="El Mundo"
-          />
-          <ItemArticle
-                  title="Artículo 3"
-                  body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-                  image="https://comercializadora.electricadecadiz.es/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png"
-                  source="https://www.google.es"
-                  publisher="Diario .es"
+            v-for="article in Articles"
+            :key="article.key"
+            :title="article.title"
+            :body="article.body"
+            :image="article.image"
+            :source="article.source"
+            :publisher="article.publisher"
           />
         </div>
       </div>
@@ -36,6 +23,7 @@
 </template>
 
 <script>
+import { db } from '../services/firebase';
 import HeaderTitle from "../components/HeaderTitle";
 import ItemArticle from "../components/ItemArticle";
 
@@ -48,6 +36,29 @@ export default {
     HeaderTitle,
     ItemArticle
   },
+
+  data() {
+    return {
+      Articles: []
+    }
+  },
+  created() {
+    db.collection('news').orderBy("date", "asc").onSnapshot((snapshotChange) => {
+      this.Articles = [];
+      snapshotChange.forEach((doc) => {
+        this.Articles.push({
+          key: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          image: doc.data().image,
+          source: doc.data().source,
+          publisher: doc.data().publisher
+        })
+      });
+    })
+  },
+  methods: {
+  }
 }
 </script>
 
