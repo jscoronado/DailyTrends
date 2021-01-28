87f86a869b974cb410531045b586d84182edb548
Jose <jose@wedocreatives.com>
2021-01-28 17:41:47 +0100

    Arreglo item article ante el caso de que el json no devuelva imagen

--- src/components/ItemArticle.vue
+++ src/components/ItemArticle.vue
@@ -2,11 +2,16 @@
   <article class="item-article col-12 col-md-3 col-lg-4 d-inline-flex">
     <div class="content-article">
       <div class="image-article">
-        <a :href="source" target="_blank"><img :src="image" :alt="title"></a>
+        <a :href="source" target="_blank">
+          <img v-if="image" :src="image" class="leaderimg" :alt="title">
+          <img v-else :src="imagenotfound" class="leaderimg" :alt="title">
+        </a>
       </div>
-      <span class="publisher-article">{{publisher}}</span>
-      <h3 class="title-article"><a :href="source" target="_blank">{{title}}</a></h3>
-      <div class="desc-article">{{body}}</div>
+      <div class="info-article">
+        <h3 class="title-article"><a :href="source" target="_blank">{{title}}</a></h3>
+        <div class="desc-article">{{body}}</div>
+        <span class="publisher-article">{{publisher}}</span>
+      </div>
     </div>
   </article>
 </template>
@@ -15,6 +20,6 @@
 
 export default {
   name: "ItemArticle",
-  props: ["title", "body", "image", "source", "publisher"],
+  props: ["title", "body", "image", "source", "publisher", "imagenotfound"],
 }
 </script>
